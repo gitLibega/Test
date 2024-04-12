a6f6130939257680cf40d1f505a21fc4215082f5
Elizaveta810 <elizaveta.k810@mail.ru>
2024-04-12 10:43:13 +0300

    Меню открывается/закрывается

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import styles from "./page.module.css";
+// import styles from "./page.module.css";
 import Bar from "@/components/Bar/Bar"
 import Navigation from "@/components/Navigation/Navigation";
 import SideBar from "@/components/SideBar/SideBar";
