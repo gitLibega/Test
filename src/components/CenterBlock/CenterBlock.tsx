45126e17bce273b5a3f4b7e8186f23a5d1f7a4d1
Elizaveta810 <elizaveta.k810@mail.ru>
2024-04-14 22:28:52 +0300

    Вынесен component/Playlist(стилизован)

--- src/components/CenterBlock/CenterBlock.tsx
+++ src/components/CenterBlock/CenterBlock.tsx
@@ -1,8 +1,8 @@
 import styles from "./CentarBlock.module.css";
 import Track from "@/components/Track/Track";
-import classNames from "classnames";
 import Filters from "../Filters/Filters";
 import Search from "../Search/Search";
+import Playlist from "../Playlist/Playlist";
 
 export default function CenterBlock() {
   return (
@@ -10,27 +10,7 @@ export default function CenterBlock() {
       <Search />
       <h2 className={styles.centerblockH2}>Треки</h2>
       <Filters />
-      <div className={styles.centerblockContent}>
-        <div className={styles.contentTitle}>
-          <div className={classNames(styles.playlistTitleCol, styles.col01)}>
-            Трек
-          </div>
-          <div className={classNames(styles.playlistTitleCol, styles.col02)}>
-            Исполнитель
-          </div>
-          <div className={classNames(styles.playlistTitleCol, styles.col03)}>
-            Альбом
-          </div>
-          <div className={classNames(styles.playlistTitleCol, styles.col04)}>
-            <svg className={styles.playlistTitleSvg}>
-              <use href="/img/icon/sprite.svg#icon-watch" />
-            </svg>
-          </div>
-        </div>
-        <div className={styles.contentPlaylist}>
-          <Track />
-        </div>
-      </div>
+      <Playlist/>
     </div>
   );
 }
