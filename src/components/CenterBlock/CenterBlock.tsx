193f539314eb81c7ec8f97e64da2a5131be97e8f
Elizaveta810 <elizaveta.k810@mail.ru>
2024-04-14 22:08:14 +0300

    Вынесен component/Search(стилизован)

--- src/components/CenterBlock/CenterBlock.tsx
+++ src/components/CenterBlock/CenterBlock.tsx
@@ -1,39 +1,36 @@
-import styles from "./CentarBlock.module.css"
+import styles from "./CentarBlock.module.css";
 import Track from "@/components/Track/Track";
 import classNames from "classnames";
 import Filters from "../Filters/Filters";
+import Search from "../Search/Search";
 
 export default function CenterBlock() {
   return (
     <div className={styles.mainCenterblock}>
-            <div className={styles.centerblockSearch}>
-              <svg className={styles.searchSvg}>
-                <use href="/img/icon/sprite.svg#icon-search" />
-              </svg>
-              <input
-                className={styles.searchText}
-                type="search"
-                placeholder="Поиск"
-                name="search"
-              />
-            </div>
-            <h2 className={styles.centerblockH2}>Треки</h2>
-            <Filters/>
-            <div className={styles.centerblockContent}>
-              <div className={styles.contentTitle}>
-                <div className={classNames(styles.playlistTitleCol, styles.col01)}>Трек</div>
-                <div className={classNames(styles.playlistTitleCol, styles.col02)}>Исполнитель</div>
-                <div className={classNames(styles.playlistTitleCol, styles.col03)}>Альбом</div>
-                <div className={classNames(styles.playlistTitleCol, styles.col04)}>
-                  <svg className={styles.playlistTitleSvg}>
-                    <use href="/img/icon/sprite.svg#icon-watch" />
-                  </svg>
-                </div>
-              </div>
-              <div className={styles.contentPlaylist}>
-                <Track/>
-              </div>
-            </div>
+      <Search />
+      <h2 className={styles.centerblockH2}>Треки</h2>
+      <Filters />
+      <div className={styles.centerblockContent}>
+        <div className={styles.contentTitle}>
+          <div className={classNames(styles.playlistTitleCol, styles.col01)}>
+            Трек
           </div>
-  )
+          <div className={classNames(styles.playlistTitleCol, styles.col02)}>
+            Исполнитель
+          </div>
+          <div className={classNames(styles.playlistTitleCol, styles.col03)}>
+            Альбом
+          </div>
+          <div className={classNames(styles.playlistTitleCol, styles.col04)}>
+            <svg className={styles.playlistTitleSvg}>
+              <use href="/img/icon/sprite.svg#icon-watch" />
+            </svg>
+          </div>
+        </div>
+        <div className={styles.contentPlaylist}>
+          <Track />
+        </div>
+      </div>
+    </div>
+  );
 }
