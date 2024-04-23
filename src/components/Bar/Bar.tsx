19ed3ee19d29484270900de2e731e5c34d069f73
Elizaveta810 <elizaveta.k810@mail.ru>
2024-04-23 14:06:28 +0300

    Bar

--- src/components/Bar/Bar.tsx
+++ src/components/Bar/Bar.tsx
@@ -1,21 +1,55 @@
 "use client";
-import { useRef } from "react";
+import { ChangeEvent, useEffect, useRef, useState } from "react";
 import styles from "./Bar.module.css";
 import classNames from "classnames";
 import { trackType } from "@/types";
+import ProgressBar from "../ProgressBar/ProgressBar";
 
 type BarType = {
   track: trackType;
 };
 
 export default function Bar({ track }: BarType) {
+  const [currentTime, setCurrentTime] = useState<number>(0);
+  const [isPlaying, setIsPlaying] = useState<boolean>(false);
   const audioRef = useRef<null | HTMLAudioElement>(null);
+
+  const duration = audioRef.current?.duration;
+
+  const togglePlay = () => {
+    if (audioRef.current) {
+      if (isPlaying) {
+        audioRef.current.pause();
+      } else {
+        audioRef.current.play();
+      }
+      setIsPlaying(!isPlaying);
+    }
+  };
+
+  useEffect(() => {
+    audioRef.current?.addEventListener("timeupdate", () =>
+      setCurrentTime(audioRef.current!.currentTime)
+    );
+  }, []);
+
+  const handleSeek = (event: ChangeEvent<HTMLInputElement>) => {
+    if (audioRef.current) {
+      setCurrentTime(Number(event.target.value));
+      audioRef.current.currentTime = Number(event.target.value);
+    }
+  };
 
   return (
     <div className={styles.bar}>
       <div className={styles.barContent}>
-        <audio ref={audioRef} src={track.track_file}></audio>
-        <div className={styles.barPlayerProgress} />
+        <audio ref={audioRef} src={track.track_file} ></audio>
+        <ProgressBar
+          max={duration}
+          value={currentTime}
+          step={0.01}
+          onChange={handleSeek}
+        />
         <div className={styles.barPlayerBlock}>
           <div className={styles.barPlayer}>
             <div className={styles.playerControls}>
@@ -24,9 +58,13 @@ export default function Bar({ track }: BarType) {
                   <use xlinkHref="img/icon/sprite.svg#icon-prev" />
                 </svg>
               </div>
-              <div className={styles.playerBtnPlay}>
+              <div onClick={togglePlay} className={styles.playerBtnPlay}>
                 <svg className={styles.playerBtnPlaySvg}>
-                  <use xlinkHref="img/icon/sprite.svg#icon-play" />
+                  <use
+                    xlinkHref={`img/icon/sprite.svg#${
+                      isPlaying ? "icon-pause" : "icon-play"
+                    }`}
+                  />
                 </svg>
               </div>
               <div className={styles.playerBtnNext}>
