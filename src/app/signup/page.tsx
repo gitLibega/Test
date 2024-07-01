775954782ba192c3fe9982dd6fa9f2a5224c2131
Elizaveta810 <elizaveta.k810@mail.ru>
2024-07-01 12:24:20 +0300

    signup

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,48 +1,80 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
 import styles from "../signup/page.module.css";
 import classNames from "classnames";
+import { useState } from "react";
+import { signup } from "@/api/signup";
+import { useUser } from "@/hooks/useUser";
 
 export default function SignupPage() {
+  const { login }: any = useUser();
+  const [signupData, setSignupData] = useState({
+    email: "",
+    username: "",
+    password: "",
+    repeatPassword: "",
+  });
+
+  const handleInputChange = (e: any) => {
+    const { name, value } = e.target;
+    setSignupData({
+      ...signupData,
+      [name]: value,
+    });
+  };
+
+  const handleSignup = async () => {
+    await signup(signupData)
+      .then((data) => {
+        login(data);
+      })
+      .catch((error) => {
+        alert(error);
+      });
+  };
   return (
     <div className={styles.wrapper}>
-    <div className={styles.containerSignup}>
-      <div className={styles.modalBlock}>
-        <form className={styles.modalFormLogin}>
-          <a href="../">
-            <div className={styles.modalLogo}>
-              <Image
-                width={140}
-                height={21}
-                src="/image/logo_modal.png"
-                alt="Логотип"
-              />
-            </div>
-          </a>
-          <input
-            className={classNames(styles.modalInput, styles.login)}
-            type="text"
-            name="login"
-            placeholder="Почта"
-          />
-          <input
-            className={classNames(styles.modalInput, styles.passwordFirst)}
-            type="password"
-            name="password"
-            placeholder="Пароль"
-          />
-          <input
-            className={classNames(styles.modalInput, styles.passwordDouble)}
-            type="password"
-            name="password"
-            placeholder="Повторите пароль"
-          />
-          <button className={styles.modalBtnSignupEnt}>
-            <Link href="/">Зарегистрироваться</Link>
-          </button>
-        </form>
+      <div className={styles.containerSignup}>
+        <div className={styles.modalBlock}>
+          <form className={styles.modalFormLogin}>
+            <a href="../">
+              <div className={styles.modalLogo}>
+                <Image
+                  width={140}
+                  height={21}
+                  src="/img/logo_modal.png"
+                  alt="Логотип"
+                />
+              </div>
+            </a>
+            <input
+              onChange={handleInputChange}
+              className={classNames(styles.modalInput, styles.login)}
+              type="text"
+              name="username"
+              placeholder="Введите имя профиля"
+            />
+            <input
+              onChange={handleInputChange}
+              className={classNames(styles.modalInput, styles.passwordFirst)}
+              type="password"
+              name="password"
+              placeholder="Пароль"
+            />
+            <input
+              onChange={handleInputChange}
+              className={classNames(styles.modalInput, styles.passwordDouble)}
+              type="password"
+              name="repeatPassword"
+              placeholder="Повторите пароль"
+            />
+            <button onClick={handleSignup} className={styles.modalBtnSignupEnt}>
+              <Link href="/">Зарегистрироваться</Link>
+            </button>
+          </form>
+        </div>
       </div>
     </div>
-  </div>
-  )
+  );
 }
--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -1,55 +1,61 @@
-import Link from "next/link"
-import styles from "./SideBar.module.css"
-import Image from "next/image"
+"use client";
+import Link from "next/link";
+import styles from "./SideBar.module.css";
+import Image from "next/image";
+import { useUser } from "@/hooks/useUser";
+import React from "react";
 
 export default function SideBar() {
+  const { user, logout } = useUser();
   return (
     <div className={styles.mainSidebar}>
-            <div className={styles.sidebarPersonal}>
-              <p className={styles.sidebarPersonalName}>Sergey.Ivanov</p>
-              <div className={styles.sidebarIcon}>
-                <svg>
-                  <use href="/img/icon/sprite.svg#logout" />
-                </svg>
-              </div>
-            </div>
-            <div className={styles.sidebarBlock}>
-              <div className={styles.sidebarList}>
-                <div className={styles.sidebarItem}>
-                  <Link className={styles.sidebarLink} href="/tracks/category/1">
-                    <Image
-                      className={styles.sidebarImg}
-                      src="/img/playlist01.png"
-                      alt="day's playlist"
-                      width={250}
-                      height={150}
-                    />
-                  </Link>
-                </div>
-                <div className={styles.sidebarItem}>
-                  <Link className={styles.sidebarLink} href="/tracks/category/2">
-                    <Image
-                      className={styles.sidebarImg}
-                      src="/img/playlist02.png"
-                      alt="day's playlist"
-                      width={250}
-                      height={150}
-                    />
-                  </Link>
-                </div>
-                <div className={styles.sidebarItem}>
-                  <Link className={styles.sidebarLink} href="/tracks/category/3">
-                    <Image
-                      className={styles.sidebarImg}
-                      src="/img/playlist03.png"
-                      alt="day's playlist"
-                      width={250}
-                      height={150}
-                    />
-                  </Link>
-                </div>
-              </div>
-            </div>
+      {user?.email && (
+        <div className={styles.sidebarPersonal}>
+          <p className={styles.sidebarPersonalName}>{user?.email}</p>
+          <div onClick={logout} className={styles.sidebarIcon}>
+            <svg>
+              <use xlinkHref="img/icon/sprite.svg#logout" />
+            </svg>
           </div>
-  )
+        </div>
+      )}
+      <div className={styles.sidebarBlock}>
+        <div className={styles.sidebarList}>
+          <div className={styles.sidebarItem}>
+            <Link className={styles.sidebarLink} href="/tracks/category/1">
+              <Image
+                className={styles.sidebarImg}
+                src="/img/playlist01.png"
+                alt="day's playlist"
+                width={250}
+                height={150}
+              />
+            </Link>
+          </div>
+          <div className={styles.sidebarItem}>
+            <Link className={styles.sidebarLink} href="/tracks/category/2">
+              <Image
+                className={styles.sidebarImg}
+                src="/img/playlist02.png"
+                alt="day's playlist"
+                width={250}
+                height={150}
+              />
+            </Link>
+          </div>
+          <div className={styles.sidebarItem}>
+            <Link className={styles.sidebarLink} href="/tracks/category/3">
+              <Image
+                className={styles.sidebarImg}
+                src="/img/playlist03.png"
+                alt="day's playlist"
+                width={250}
+                height={150}
+              />
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
