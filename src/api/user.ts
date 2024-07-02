0fe8239e938552a7df6dd5b9eeaad6e971f0258b
Elizaveta810 <elizaveta.k810@mail.ru>
2024-07-02 17:56:58 +0300

    запрос для получения token

--- src/api/user.ts
+++ src/api/user.ts
@@ -2,7 +2,7 @@ type SigninFormType = {
   email: string;
   password: string;
 }
-export  const  fetchUser = async ({email, password}: SigninFormType) => {
+export  async function fetchUser({email, password}: SigninFormType) {
   const response = await fetch (
     "https://skypro-music-api.skyeng.tech/user/login/",
     {
@@ -11,6 +11,9 @@ export  const  fetchUser = async ({email, password}: SigninFormType) => {
         email,
         password,
       }),
+      headers: {
+        "Content-Type": "application/json"
+      },
     }
   );
   if (response.status === 400) {
@@ -21,4 +24,27 @@ export  const  fetchUser = async ({email, password}: SigninFormType) => {
   const responseData = await response.json();
   return responseData; 
 
+}
+
+export  async function fetchToken({email, password}: SigninFormType) {
+  const response = await fetch (
+    "https://skypro-music-api.skyeng.tech/user/token/",
+    {
+      method: "POST",
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+      headers: {
+        "Content-Type": "application/json"
+      },
+    }
+  );
+  if (response.status === 400) {
+    throw new Error ("Неверный пароль или логин");
+  } else if (!response.ok) {
+    throw new Error ("Заполните поля");
+  }
+  const responseData = await response.json();
+  return responseData; 
 }
